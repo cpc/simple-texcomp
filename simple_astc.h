320735b595ddd16208d199a7ac4b42b08ed5a940
Jakub Žádník <jakub.zadnik@tuni.fi>
2021-03-19 19:06:58 +0200

    Implement packing into the ASTC format
    This assumes weights after decimation and quantization without using
    trits/quints

--- simple_astc.h
+++ simple_astc.h
@@ -78,15 +78,139 @@ int store_astc_image(
 
 /* ========================================================================= */
 
+// Taken from astcenc:
+// routine to write up to 8 bits
+static inline void write_bits(
+	int value,
+	int bitcount,
+	int bitoffset,
+	uint8_t* ptr
+) {
+	int mask = (1 << bitcount) - 1;
+	value &= mask;
+	ptr += bitoffset >> 3;
+	bitoffset &= 7;
+	value <<= bitoffset;
+	mask <<= bitoffset;
+	mask = ~mask;
+
+	ptr[0] &= mask;
+	ptr[0] |= value;
+	ptr[1] &= mask >> 8;
+	ptr[1] |= value >> 8;
+}
+
+/* Taken from astcenc */
+static inline int bitrev8(int p)
+{
+	p = ((p & 0xF) << 4) | ((p >> 4) & 0xF);
+	p = ((p & 0x33) << 2) | ((p >> 2) & 0x33);
+	p = ((p & 0x55) << 1) | ((p >> 1) & 0x55);
+	return p;
+}
+
+void print_bin(unsigned int num, unsigned int nb)
+{
+	for (uint b = 0; b < nb; ++b)
+	{
+		unsigned int x = (num >> (nb-1-b)) & 1;
+		printf("%d", x);
+	}
+}
+
 void encode_block_astc(
     const uint8_t block_pixels[NCH_RGB*ASTC_BLOCK_X*ASTC_BLOCK_Y],
     uint32_t out[4]
 ){
-    uint8_t encoded_block_bytes[16] = {
-        209, 80, 60, 7, 82, 165, 230, 115, 164, 113, 51, 128, 8, 204, 179, 179
+    const uint16_t block_mode = 102;
+    const uint8_t wgt_grid_w = 8;
+    const uint8_t wgt_grid_h = 5;
+    const uint8_t wgt_count = wgt_grid_w * wgt_grid_h;
+
+    const uint8_t color_quant_level = 11;  // range 32
+    const uint8_t ep_bits = 5;
+    const uint8_t wgt_quant_mode = 2;      // range 4
+    const uint8_t wgt_bits = 2;
+
+    const int wgt_bitcount = wgt_count * wgt_bits;
+
+    // Weights after decimation
+    const uint8_t weights[wgt_count] = {  // wgt_count == 40
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+        3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
     };
 
-    memcpy(out, encoded_block_bytes, 16);
+	uint8_t wgt_buf[16];
+	uint8_t out_buf[16];
+    for (int i = 0; i < 16; ++i)
+    {
+        wgt_buf[i] = 0;
+        out_buf[i] = 0;
+    }
+
+    // ISE encoding
+    int off = 0;
+    for (int i = 0; i < wgt_count; ++i)
+    {
+        write_bits(weights[i], wgt_bits, off, wgt_buf);
+        off += wgt_bits;
+    }
+
+    // write out weights
+    for (int i = 0; i < 16; ++i)
+    {
+        out_buf[i] = bitrev8(wgt_buf[15-i]);
+    }
+
+    // write out mode, partition, CEM
+    write_bits(block_mode, 11, 0, out_buf);
+    const int partition_count = 1;
+    write_bits(partition_count - 1, 2, 11, out_buf);
+    const int color_format = 8;
+    write_bits(color_format, 4, 13, out_buf);
+
+    // Unquantized endpoints
+    const uint8_t mincol[3] = { 0, 0, 0 };
+    const uint8_t maxcol[3] = { 255, 255, 255 };
+    if (mincol[0] + mincol[1] + mincol[2] > maxcol[0] + maxcol[1] + maxcol[2])
+    {
+        printf("ERROR: mincol must be smaller than maxcol\n");
+    }
+
+    // Quantized endpoints (layout is R0 R1 G0 G1 B0 B1)
+    uint8_t endpoints_q[6] = { 0 };
+    for (int i = 0; i < 3; ++i)
+    {
+        endpoints_q[2*i] = mincol[i] >> 3;
+        endpoints_q[2*i+1] = maxcol[i] >> 3;
+    }
+
+    // write out endpoints
+    off = 17;  // starting bit position for endpoints data
+    for (int i = 0; i < 6; ++i)
+    {
+        printf("ep[%d]: %d\n", i, endpoints_q[i]);
+        write_bits(endpoints_q[i], ep_bits, off, out_buf);
+        off += ep_bits;
+    }
+
+    printf("Output block (msb..lsb):\n");
+    for (int i = 0; i < 16; ++i)
+    {
+        printf("i: %2d  wgt: ", i);
+        print_bin(wgt_buf[i], 8);
+        printf("  out: ");
+        print_bin(out_buf[i], 8);
+        printf("\n");
+    }
+
+    // uint8_t encoded_block_bytes[16] = {
+    //     209, 80, 60, 7, 82, 165, 230, 115, 164, 113, 51, 128, 8, 204, 179, 179
+    // };
+
+    memcpy(out, out_buf, 16);
 }
 
 // void decode_block_astc(
