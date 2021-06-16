fede78b3d457736af445d5c319076cb9d2ce0394
Jakub Žádník <jakub.zadnik@tuni.fi>
2021-06-16 14:44:51 +0300

    Add fixed-point division approximation

--- scripts/divtest.py
+++ scripts/divtest.py
@@ -28,7 +28,6 @@ def approx_newton_float(x, n):
     # Initial estimate
     B = 48.0 / 17.0
     A = 32.0 / 17.0
-    print(A, B)
     init = B - A * x_sc
 
     # Newthon-Raphson iterations
@@ -38,35 +37,31 @@ def approx_newton_float(x, n):
         y1 = 2.0 * y0 - x_sc * y0 * y0
         y0 = y1
 
-    return y1, sc
+    return y1*sc, sc
 
 
-def fixed(x):
+def fixed(x, s=0, w=24, f=16, rounding='round', overflow='saturate', fixed=True):
     if not isinstance(x, Iterable):
         x = [x]
-    return numfi(x, s=0, w=8, f=8, rounding='round', overflow='saturate', fixed=True)
+    return numfi(x, s=s, w=w, f=f, rounding=rounding, overflow=overflow, fixed=fixed)
 
 
 def approx_newton_fixed(x, n):
-    #  if x == 0.0:
-    #      return 0.0
-
     # Scale x to be within [0.5, 1.0]
     x_sc = fixed(np.copy(x))
-    sc = fixed(np.ones_like(x))
+    sc = np.zeros_like(x, dtype=int)
     for i, xi in enumerate(x_sc):
         while x_sc[i] < 0.5:
-            x_sc[i] = x_sc[i] << 2
-            sc[i] = sc[i] << 2
+            x_sc[i] = x_sc[i] << 1
+            sc[i] += 1
 
         while x_sc[i] > 1.0:
-            x_sc[i] = x_sc[i] >> 2
-            sc[i] = sc[i] >> 2
+            x_sc[i] = x_sc[i] >> 1
+            sc[i] -= 1
 
     # Initial estimate
     A = fixed(32.0 / 17.0)
     B = fixed(48.0 / 17.0)
-    print(A, B)
     init = B - A * x_sc
 
     # Newthon-Raphson iterations
@@ -76,43 +71,36 @@ def approx_newton_fixed(x, n):
         y1 = fixed(2.0) * y0 - x_sc * y0 * y0
         y0 = y1
 
-    return y1, sc
+    return y1<<sc, sc
 
 
 if __name__ == '__main__':
     pd.set_option("display.precision", 10)
 
-    n = 256
-    step = 8
+    n = 256 * 3
+    step = 16
     series = np.arange(1, n, step)
 
-    x = series / n
-
-    approx, sc = approx_newton_float(x, 1)
-
+    x = series / 256
     gt = 1.0 / x
-    gt /= sc
 
-    xfi = numfi(x, s=0, w=8, f=8, rounding='round', overflow='saturate', fixed=True)
-    approxfi, scfi = approx_newton_fixed(x, 1)
+    n_iter = 2
+    print('iterations: {}'.format(n_iter))
+    approx, sc = approx_newton_float(x, n_iter)
 
-    df = pd.DataFrame(data=np.array([x, sc, xfi, gt, approx, approxfi]).T, columns=['x', 'sc', 'xfi', 'gt', 'flt_newt', 'fi_newt'])
+
+    xfi = fixed(x)
+    approxfi, scfi = approx_newton_fixed(xfi, n_iter)
+
+    df = pd.DataFrame(data=np.array([x, sc, scfi, xfi, gt, approx, approxfi]).T,
+        columns=['x', 'sc', 'scfi', 'xfi', 'gt', 'flt_newt', 'fi_newt'])
     print(df)
 
-    #  print("x,gt,approx,diff")
-    #  for i in range(0, n+1, step):
-    #      x = i / n * 3
-    #      x_orig = x
-    #      if x == 0:
-    #          continue
-
-    #      approx, sc = approx_newton_float(x, 2)
-
-    #      gt = 1.0 / x
-    #      gt /= sc
-
-    #      print("{:.20f},{:.20f},{:.20f},{:.20f}".format(x_orig, gt, approx, gt-approx))
-
-    num = 1.0
-    fi = numfi([num], s=0, w=8, f=8, rounding='round', overflow='saturate', fixed=True)
-    print(num, fi, fi.bin, fi.bin_, fi.hex, fi.precision)
+    nums = [0.01, 0.1, 0.2, 0.25, 0.4, 0.49, 0.5, 1.0, 128.0]
+    fi = fixed(nums, w=16, f=8)
+    print(nums)
+    print(fi)
+    print(fi.bin)
+    print(fi.bin_)
+    print(fi.hex)
+    print(fi.precision)
